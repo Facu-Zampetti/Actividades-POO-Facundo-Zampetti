9b9dbdee86e7209f12a7ce75f96f6b136e69af84
FacundoZampetti <141205028+Facu-Zampetti@users.noreply.github.com>
2026-04-28 12:42:42 -0300

    Add files via upload

---
+++ Actividad05 (1 de abril)/Ejercicio5 (lienzo) FACUNDO ZAMPETTI/Ejercicio5/mainwindow.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <QMainWindow>
+#include <QHash>
+#include <QString>
+
+class CanvasWidget;
+class SyncService;
+class RealtimeSyncService;
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+public:
+    explicit MainWindow(QWidget* parent = nullptr);
+
+private:
+    CanvasWidget* m_canvas = nullptr;
+    SyncService* m_sync = nullptr;
+    RealtimeSyncService* m_rt = nullptr;
+    QString m_localAuthor;
+
+    // strokeId -> index en m_canvas->model().strokes()
+    QHash<QString, int> m_remoteInProgressIndex;
+};
